ac5975c7f36f86e1be6fc06e4fb1ae43eafa0251
Antoine <antoine.lorin2@gmail.com>
2021-11-03 15:20:16 +0100

    enh(router): add yaml support

--- www/index.php
+++ www/index.php
@@ -3,30 +3,5 @@
 require_once "controllers/Router.php";
 require_once "vue/Index.php";
 
-
-function sendLastRelease()
-{
-    header('Content-Disposition: attachment; filename="Dockerfile"');
-    readfile("./public/Dockerfile");
-}
-
-function indexPage()
-{
-    $page = new Index("Index");
-    $page->render();
-}
-
-function getRoutes()
-{
-    yaml_parse_file("./routes.yml");
-}
-
 $router = new Router();
-$routes = getRoutes();
-
-
-$router->addRoute("/release", "sendLastRelease");
-$router->addRoute("/", "indexPage");
-
-
 $router->route($_GET["path"]);
