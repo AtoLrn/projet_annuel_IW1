c8d2be439674b133f17e4f6ed251256fcc60f784
Yves skrzypczyk <y.skrzypczyk@gmail.com>
2021-12-01 12:58:11 +0100

    tp tpl correction

--- www/Controller/Main.class.php
+++ www/Controller/Main.class.php
@@ -2,11 +2,19 @@
 
 namespace App\Controller;
 
+use App\Core\View;
+
 class Main {
 
     public function home()
     {
         echo "Page d'accueil";
+    }
+
+
+    public function contact()
+    {
+        $view = new View("contact");
     }
 
 
