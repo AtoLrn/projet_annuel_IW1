2816e47f4865e18577fcc8b5595fed1599f0b40c
Antoine <antoine.lorin2@gmail.com>
2022-01-15 14:38:14 +0100

    enh(editorjs): add librairy import

--- www/View/front.tpl.php
+++ www/View/front.tpl.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <title>Template FRONT</title>
     <meta name="description" content="Description de ma page">
+    <script src="https://cdn.jsdelivr.net/npm/@editorjs/editorjs@latest"></script>
+
 </head>
+
 <body>
 
-    <?php include "View/".$this->view.".view.php"; ?>
+    <?php include "View/" . $this->view . ".view.php"; ?>
 
 </body>
+
 </html>
