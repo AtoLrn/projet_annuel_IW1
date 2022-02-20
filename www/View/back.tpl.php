43611a93cb0da86f0ffccfc8fd4fb0013a2a3265
Antoine <antoine.lorin2@gmail.com>
2022-02-20 16:54:07 +0100

    feat(navbar): creation of the nabvar css

--- www/View/back.tpl.php
+++ www/View/back.tpl.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <title>Template BACK</title>
@@ -8,27 +9,33 @@
     <link rel="stylesheet" href="../assets/dist/main.css">
     <script type="text/javascript" src="../assets/dist/main.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.css">
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js">
+    </script>
 </head>
+
 <body>
     <div class="grid">
         <header class="navbar p-6 grid">
             <div class="col g-5">
-                <a href=""><img src="../assets/img/logo/croc-miam.svg" alt=""> </a>
+                <a href=""><img src="../assets/img/logo/croc-miam.svg" alt=""></a>
                 <div class="bd-t-1 bd-light-gray mb-11"></div>
                 <nav class="grid">
                     <ul class="col g-9 grid">
-                        <li class="row g-3"> <img src="../assets/img/logo/users.svg" alt=""> <a href="#" class="link-black">Utilisateurs</a> </li>
-                        <li class="row g-3"> <img src="../assets/img/logo/articles.svg" alt=""> <a href="#" class="link-black">Articles</a> </li>
-                        <li class="row g-3"> <img src="../assets/img/logo/comments.svg" alt=""> <a href="#" class="link-black">Commentaires</a> </li>
+                        <li class="row g-3"> <img src="../assets/img/logo/users.svg" alt=""> <a href="#"
+                                class="link-black">Utilisateurs</a> </li>
+                        <li class="row g-3"> <img src="../assets/img/logo/articles.svg" alt=""> <a href="#"
+                                class="link-black">Articles</a> </li>
+                        <li class="row g-3"> <img src="../assets/img/logo/comments.svg" alt=""> <a href="#"
+                                class="link-black">Commentaires</a> </li>
                     </ul>
                 </nav>
                 <img class="burger-menu" src="../assets/img/logo/burger-menu.svg" alt="">
             </div>
-        </header >
-        <main class="grid main"> 
-            <?php include "View/".$this->view.".view.php"; ?>
+        </header>
+        <main class="grid main">
+            <?php include "View/" . $this->view . ".view.php"; ?>
         </main>
     </div>
 </body>
+
 </html>
--- www/View/front.tpl.php
+++ www/View/front.tpl.php
@@ -6,11 +6,25 @@
     <title>Template FRONT</title>
     <meta name="description" content="Description de ma page">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../assets/css/dist/main.css">
+    <link rel="stylesheet" href="../assets/dist/main.css">
     <script src="https://cdn.jsdelivr.net/npm/@editorjs/editorjs@latest"></script>
 </head>
 
 <body>
+    <header class="navbar-front grid h-px-20 px-20 mb-5">
+        <nav class="row j-bet">
+            <div><img height="52px" src="assets/img/logo/croc-miam.svg" alt=""></div>
+            <ul>
+                <li>Nos Recettes</li>
+                <li>Les Cuisiniers</li>
+                <li>Vos Abonnements</li>
+            </ul>
+            <section>
+            <a class="link" href="/login"><img src="../assets/img/logo/users.svg" alt="" width="24px">Se Connecter</a>
+            <a class="link" href="/register"><img src="../assets/img/logo/users.svg" alt="" width="24px">S'inscrire</a>
+            </section>
+        </nav>
+    </header>
     <?php include "View/" . $this->view . ".view.php"; ?>
 </body>
 
